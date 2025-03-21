a3a25b61f35dfb4000dbaae61457b55680b30a3c
MinWoo <cmahsh8756@gmail.com>
2025-03-21 11:09:44 +0900

    복구

---
+++ Source/SuperHot_Copy/Private/HS/Weapons/Bat.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HS/Weapons/Bat.h"
+
+ABat::ABat()
+{
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>BatMesh(TEXT("/Script/Engine.StaticMesh'/Game/HS/assets/Bat/source/BaseballBat_Low.BaseballBat_Low'"));
+	if(BatMesh.Succeeded())
+	{
+		WeaponMesh->SetStaticMesh(BatMesh.Object);
+	}
+	
+}
+
+void ABat::Fire_Implementation()
+{
+	FHitResult HitResult;
+	FVector Start = MeshComp->GetSocketLocation("Start");
+	FVector End = Start + MeshComp->GetForwardVector() * Range;
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, QueryParams))
+	{
+		AActor* HitActor = HitResult.GetActor();	// 데미지 적용 로직
+	}
+}
+
